5952b056e0f6d888fe8f29076f417c4aaf31ae0b
Nick Wynja <nick.wynja@infor.com>
2018-03-27 10:29:26 -0400

    Source Sans font files

--- build/scripts/package-fonts.sh
+++ build/scripts/package-fonts.sh
@@ -5,17 +5,16 @@ FILENAME="SourceSansPro"
 DEST="font"
 
 echo "Starting fresh..."
-rm -r $DEST/*.zip
+rm $DEST/{*.css,*.txt}
+rm -r $DEST/*/
+mv $DEST/README.md $DEST/README.bk
 echo "Downloading Source Sans Pro..."
 mkdir -p $DEST
-wget --quiet --output-document "$DEST/$FILENAME-ALL.zip" "https://github.com/adobe-fonts/source-sans-pro/archive/$VERSION_TAG.zip"
+wget --quiet --output-document "$DEST/$FILENAME.zip" "https://github.com/adobe-fonts/source-sans-pro/archive/$VERSION_TAG.zip"
 echo "Unzipping..."
-unzip -q -d "$DEST/formats" "$DEST/$FILENAME-ALL.zip" && f=("$DEST/formats"/*) && mv "$DEST/formats"/*/* "$DEST/formats" && rmdir "${f[@]}"
-for d in $DEST/formats/*/ ; do
-  filetype=$(echo "$d" | rev | cut -d"/" -f2  | rev)
-  echo "Packaging $filetype..."
-  zip -q -j -r $DEST/$FILENAME-$filetype.zip $DEST/formats/$filetype/*
-done
+unzip -q -d "$DEST" "$DEST/$FILENAME.zip" && f=("$DEST"/*/) && mv "$DEST"/*/* "$DEST/" && rm -r "${f}"
 echo "Cleaning up..."
-rm -r $DEST/formats
+rm -r $DEST/$FILENAME.zip
+rm $DEST/bower.json
+mv $DEST/README.bk $DEST/README.md
 echo Done!
