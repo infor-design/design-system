b667dee9f46caee04b2c5e6fb61a5b4f84da9ceb
Nick Wynja <nick.wynja@infor.com>
2018-03-28 10:46:30 -0400

    Move zip out of dist

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -2,8 +2,8 @@
 
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-mkdir -p dist && cd dist
-zip -r IDS-$PACKAGE_VERSION.zip \
+cd dist
+zip -r ../IDS-$PACKAGE_VERSION.zip \
     ../font/ \
     ../sketch/ \
     tokens/
