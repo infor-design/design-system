1cf4bc901b9ba2994b9132bc7367558c1ef78cc5
Nick Wynja <nick.wynja@infor.com>
2018-03-28 11:00:32 -0400

    Adds comments

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -2,6 +2,15 @@
 
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
+# @output zip
+# @comment cd into dist and get font and sketch
+#          from parent to build structure as below
+# @structure:
+#    IDS-X.X.X.zip
+#        font/
+#        sketch/
+#        tokens/
+
 cd dist
 zip -r ../IDS-$PACKAGE_VERSION.zip \
     ../font/ \
