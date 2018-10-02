50a5c1c490edc1071d7e2e4804ccfb2243a069c6
Christopher Lepore <chris.lepore@infor.com>
2018-10-02 13:20:34 -0400

    Add new tokens structure as "properties" for now

--- scripts/node/generate-tokens.js
+++ scripts/node/generate-tokens.js
@@ -2,7 +2,7 @@
 
 /**
  * @fileoverview This script uses theo to generate
- * different types of out from design tokens.
+ * different types of output from design tokens.
  * https://github.com/salesforce-ux/theo
  *
  * NOTE: More than likely there is a command in the package.json
@@ -12,117 +12,54 @@
 // -------------------------------------
 //   Node Modules/Options
 // -------------------------------------
-const argv = require('minimist')(process.argv.slice(2));
-const chalk = require('chalk');
+const glob = require('glob');
 const fs = require('fs');
-const glob = require('glob-fs')({ gitignore: true });
-const mkdirp = require('mkdirp');
+const swlog = require('./utilities/stopwatch-log.js');
 const path = require('path');
-const pkg = require(path.join(process.cwd(), 'package.json'));
-const rename = require('rename');
-const swLog = require('./utilities/stopwatch-log.js');
-const theo = require('theo');
-
 
 // -------------------------------------
 //   Constants/Variables
 // -------------------------------------
-const banner = `
-/**
- * ${pkg.name} - ${pkg.description}
- *
- * @version ${pkg.version}
- * @homepage ${pkg.repository.url}
- * @license ${pkg.license}
- */
-`;
-
-const distPath = path.join(process.cwd(), './dist/tokens/web');
-const tokensPath = './design-tokens';
-const formatArr = getFormats(argv.format);
-
-let tokenFiles = `${tokensPath}/*.yml`;
-if (argv.files) {
-  tokenFiles = argv.files;
-}
+const configGlob = './design-properties/**/*.config.json';
+const configs = glob.sync(configGlob);
 
 // -------------------------------------
 //   Main
 // -------------------------------------
-swLog.logTaskStart('creating tokens');
-createDirs(distPath);
+const taskName = swlog.logTaskStart('creating formats');
 
-glob.readdir(tokenFiles, (err, files) => {
-  files.forEach(file => {
-    formatArr.forEach(format => {
-      convertFileToFormat(file, format);
-    });
-  });
+configs.forEach(config => {
+  const themeName = path.basename(config, '.config.json');
+  const themeConfig = JSON.parse(fs.readFileSync(config, 'utf8'));
+
+  const platforms = {
+    scss: {
+      transformGroup: "scss",
+      buildPath: "dist/tokens/web/",
+      files: [{
+        destination: `${themeName}.scss`,
+        format: "scss/variables"
+      },
+      {
+        destination: `${themeName}.json`,
+        format: "json"
+      },
+      {
+        destination: `${themeName}.custom-properties.css`,
+        format: "css/variables"
+      }]
+    }
+  };
+
+  themeConfig.platforms = platforms;
+
+  const dict = require('style-dictionary').extend(themeConfig);
+
+  try {
+    dict.buildAllPlatforms();
+  } catch(e) {
+    console.error(e);
+  }
 });
 
-// -------------------------------------
-//   Functions
-// -------------------------------------
-
-/**
- * Convert token files into a specific format
- * @param {string} srcFile - The file to convert
- * @param {string} toFormat - The format to conver to
- */
-function convertFileToFormat(srcFile, toFormat) {
-  theo.registerTransform("web", ["color/hex"]);
-
-  theo
-    .convert({
-      transform: {
-        type: 'web',
-        file: srcFile
-      },
-      format: {
-        type: toFormat,
-      }
-    })
-    .then(res => {
-      let newFile = rename(srcFile, {
-        dirname: distPath,
-        extname: `.${toFormat}`
-      });
-
-      let contents = res;
-
-      // Do not add comment to invalidate .json files
-      if (toFormat.substr('json') > -1) {
-        contents = banner + res;
-      }
-
-      fs.writeFile(newFile, contents, (err) => {
-        if (err) {
-          throw err;
-        }
-        swLog.logTaskAction('Created', `${path.parse(newFile).base}.`);
-      });
-    })
-    .catch(error => {
-      swLog.logTaskAction('!', `${error} in ${srcFile}`, color = 'red')
-    });
-}
-
-/**
- * Create directories in a string path
- * @param {string} path
- */
-function createDirs(path) {
-  mkdirp(path, function (err) {
-    if (err) console.error(err)
-  });
-}
-
-/**
- * Get the comma separated list of formats
- * to convert to
- * @param {string} formats
- * @returns {array}
- */
-function getFormats(formats = 'raw.json') {
-  return formats.split(',');
-}
+swlog.logTaskEnd(taskName);
