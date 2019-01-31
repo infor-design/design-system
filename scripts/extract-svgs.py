9f28fa6c6bcc37c6031f4b6fa1d90a91e10ba880
Nick Wynja <nick@nickwynja.com>
2019-01-31 16:17:32 -0500

    Use EP files locally

--- scripts/extract-svgs.py
+++ scripts/extract-svgs.py
@@ -1,12 +1,15 @@
-from bs4 import BeautifulSoup as bs
-import re, json
+import re, json, os
 
 metadata = []
 
-for d in ['standard', 'extended']:
-    with open("./icons/theme-soho/src/%s.html" % d) as file:
+for d in ['svg', 'svg-extended']:
+    with open("../enterprise/src/components/icons/%s.html" % d) as file:
         data = file.read()
+        symbol_count = data.count("<symbol")
+        print(symbol_count, "icons in %s" % d)
         svgs = [x.strip() for x in data.split("\n\n")]
+        del svgs[0:1] # Delete opening divs
+        del svgs[-2:] # Delete closing divs
         l = []
         for s in svgs:
             match = re.search('id=\"icon-(.*?)\"', s)
@@ -17,11 +20,15 @@ for d in ['standard', 'extended']:
             # Brute force pretty indentation
             prettySVG = re.sub('    ', '  ', svg)
             prettySVG = re.sub('  </svg>', '</svg>\n', prettySVG)
-            print("Exporting %s/%s.svg" % (d, name))
-            f = open( 'icons/theme-soho/svg/%s/%s.svg' % (d, name), 'w+' )
+            # print("Exporting %s/%s.svg" % (d, name))
+            path = 'tmp/%s' % d
+            if not os.path.exists(path):
+                os.makedirs(path)
+            f = open( '%s/%s.svg' % (path, name), 'w+' )
             f.write(prettySVG)
             f.close()
         metadata.append({"name": d, "icons": l})
-m = open( 'icons/theme-soho/metadata.json', 'w+' )
+    print(len(os.listdir(path)), "icons in %s" % d)
+m = open( 'tmp/metadata.json', 'w+' )
 json.dump(metadata, m, indent=4)
 m.close()
