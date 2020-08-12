54fee195d4e5c2493a9183e330116409cc549ceb
Tim McConechy <tim.mcconechy@infor.com>
2020-08-12 12:22:31 -0400

    Add missing lint file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:json/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
