361cb357de4be6dbeef5bce993b8482c9e63d1fc
corpulent <artemgolub@gmail.com>
2022-09-26 16:23:11 +0300

    cd: use hl registry, minor cleanup

--- cd/run.sh
+++ cd/run.sh
@@ -9,30 +9,43 @@ set -e
 export TERM=xterm
 export NODE_OPTIONS=--max_old_space_size=4096
 
-_NPM_TOKEN=${NPM_TOKEN:-}
-_GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN:-}
+check_required_vars()
+{
+    var_names=("$@")
+    for var_name in "${var_names[@]}"; do
+        [ -z "${!var_name}" ] && echo "$var_name is unset." && var_unset=true
+    done
+    [ -n "$var_unset" ] && exit 1
+    return 0
+}
+
+check_required_vars \
+  GITHUB_ACCESS_TOKEN \
+  NPM_TOKEN \
+  BRANCH \
+  REPO_OWNER_NAME
+
 _RELEASE_INCREMENT=${RELEASE_INCREMENT:-}
-_RELEASEIT_FLAGS=${RELEASEIT_FLAGS:-}
 
 rm -fr /root/design-system/{..?*,.[!.]*,*} 2>/dev/null
 
 #echo "[url \"git@github.com:\"]\n\tinsteadOf = https://github.com/" >> /root/.gitconfig
-#git config --global url."https://${_GITHUB_ACCESS_TOKEN}:@github.com/".insteadOf "https://github.com/"
+#git config --global url."https://${GITHUB_ACCESS_TOKEN}:@github.com/".insteadOf "https://github.com/"
 
-git clone https://${_GITHUB_ACCESS_TOKEN}@github.com/infor-design/design-system.git /root/design-system
+git clone https://${GITHUB_ACCESS_TOKEN}@github.com/infor-design/design-system.git /root/design-system
 cd /root/design-system
-git remote set-url origin https://${_GITHUB_ACCESS_TOKEN}@github.com/infor-design/design-system.git
+git remote set-url origin https://${GITHUB_ACCESS_TOKEN}@github.com/infor-design/design-system.git
 
-npm config set '//registry.npmjs.org/:_authToken' "${_NPM_TOKEN}"
+npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 npm install
 npm run build
 
-if [ -n "$_RELEASEIT_FLAGS" ];
+if [ -n "$RELEASEIT_FLAGS" ];
 then
-  release-it $_RELEASEIT_FLAGS --config .release-it.json --ci -- $_RELEASE_INCREMENT
+  release-it $RELEASEIT_FLAGS --config .release-it.json --ci -- $_RELEASE_INCREMENT
 fi
 
-if [[ "$_RELEASEIT_FLAGS" == *"--dry-run=false"* ]];
+if [[ "$RELEASEIT_FLAGS" == *"--dry-run=false"* ]];
 then
     ZIP_FILES=`find . -iname \*.zip`
 
